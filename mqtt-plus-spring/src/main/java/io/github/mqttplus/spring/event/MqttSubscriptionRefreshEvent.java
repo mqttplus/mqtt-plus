07bb020e0b39c6f4bb7decfa3cf61e7ca00475b0
whugeomatics <whugeomatics@gmail.com>
2026-04-10 14:51:33 +0800

    use record pattern and modify MqttListenerAnnotationProcessor.java to Ignore AOP infrastructure

--- mqtt-plus-spring/src/main/java/io/github/mqttplus/spring/event/MqttSubscriptionRefreshEvent.java
+++ mqtt-plus-spring/src/main/java/io/github/mqttplus/spring/event/MqttSubscriptionRefreshEvent.java
@@ -1,37 +1,11 @@
 package io.github.mqttplus.spring.event;
 
-public final class MqttSubscriptionRefreshEvent {
+public record MqttSubscriptionRefreshEvent(Action action,
+                                           String brokerId, String topic, int qos) {
 
     public enum Action {
         SUBSCRIBE,
         UNSUBSCRIBE
     }
 
-    private final Action action;
-    private final String brokerId;
-    private final String topic;
-    private final int qos;
-
-    public MqttSubscriptionRefreshEvent(Action action, String brokerId, String topic, int qos) {
-        this.action = action;
-        this.brokerId = brokerId;
-        this.topic = topic;
-        this.qos = qos;
-    }
-
-    public Action getAction() {
-        return action;
-    }
-
-    public String getBrokerId() {
-        return brokerId;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public int getQos() {
-        return qos;
-    }
 }
